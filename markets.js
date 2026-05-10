1a761fef785c1b1e7aab6ab495c3d1347f19dc92
brviscase-blip <brviscase@gmail.com>
2026-05-10 00:11:51 -0400

    fix: use REST API instead of JSON-RPC

--- markets.js
+++ markets.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const axios = require('axios');
 const { getToken } = require('./auth');
 
-const API_URL = 'https://api.betfair.com/exchange/betting/json-rpc/v1';
+const API_URL = 'https://api.betfair.com/exchange/betting/rest/v1.0';
 
 async function apiCall(method, params) {
   const token = await getToken();
   const response = await axios.post(
-    API_URL,
-    [{ jsonrpc: '2.0', method: `SportsAPING/v1.0/${method}`, params, id: 1 }],
+    `${API_URL}/${method}/`,
+    params,
     {
       headers: {
         'X-Application': process.env.BETFAIR_APP_KEY,
@@ -17,7 +17,7 @@ async function apiCall(method, params) {
       },
     }
   );
-  return response.data[0].result;
+  return response.data;
 }
 
 // Busca jogos de futebol pré-jogo nas próximas 24h
